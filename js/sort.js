3a5d376ce353e83f883f07703261a934acd52577
thenotoriousob <32715357+thenotoriousob@users.noreply.github.com>
2024-08-21 10:45:04 +0100

    Delete js/sort.js

--- js/sort.js
+++
@@ -1,8 +0,0 @@
-export const renderSort = () => {
-    return `
-        <div class="view-mode">
-            <i class="fa-solid fa-table-cells" tabindex="0"></i>
-            <i class="fa-solid fa-list" tabindex="0"></i>
-        </div>
-    `;
-};

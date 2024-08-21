2508c282ff596069860f3fc77549f584291da217
thenotoriousob <32715357+thenotoriousob@users.noreply.github.com>
2024-08-21 10:44:49 +0100

    Delete js/search-bar.js

--- js/search-bar.js
+++
@@ -1,8 +0,0 @@
-export const renderSearchBar = () => {
-    return `
-        <section class="search-container">
-            <input type="input" class="search-input" placeholder="Search for a movie" />
-            <button type="button" class="search-btn">Search</button>
-        </section>
-    `;
-};

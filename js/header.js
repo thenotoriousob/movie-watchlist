6d7e36743a778706e4a5b8184c52e81d4bfde7b1
thenotoriousob <32715357+thenotoriousob@users.noreply.github.com>
2024-08-21 10:44:22 +0100

    Delete js/header.js

--- js/header.js
+++
@@ -1,8 +0,0 @@
-export const renderHeader = (route) => {
-    return `
-        <div class="container">
-            <h1>${route.title}</h1>
-            <a class="links watchlist-link" href="./${route.linkPath}">${route.linkLabel}</a>
-        </div>
-    `;
-}
